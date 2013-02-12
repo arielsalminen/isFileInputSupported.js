d04209220d620480d33d586f018c52173fdb4963
Viljami S <viljami.salminen@gmail.com>
2013-02-12 21:23:35 +0200

    Add missing brackets

--- isFileInputSupported.js
+++ isFileInputSupported.js
@@ -1,7 +1,7 @@
 // Detect input type=file support
 var isFileInputSupported = (function () {
   // Handle devices which falsely report support
-  if (navigator.userAgent.match(/(Android (1.0|1.1|1.5|1.6|2.0|2.1))|(Windows Phone (OS 7|8.0)|(XBLWP)|(ZuneWP)|(w(eb)?OSBrowser)|(webOS)|Pre\/1.2|Kindle\/(1.0|2.0|2.5|3.0))/)) {
+  if (navigator.userAgent.match(/(Android (1.0|1.1|1.5|1.6|2.0|2.1))|(Windows Phone (OS 7|8.0))|(XBLWP)|(ZuneWP)|(w(eb)?OSBrowser)|(webOS)|(Kindle\/(1.0|2.0|2.5|3.0))/)) {
     return false;
   }
   // Create test element
